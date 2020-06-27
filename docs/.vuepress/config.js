a9c57a08d6efe2a67d4bae27b7d56daee93429ba
ince4 <421603832@qq.com>
2020-06-27 18:45:43 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'ince4的前端笔记',
+	description: '记录、整理学习过程中的知识点',
+	themeConfig:{
+		nav: [{text: "", link: ""},
+		  ],
+	  }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /animal_alpaca_suri.png
+actionText: Get Started →
+actionLink: /front-end/
+
+---
